13d7717214d3d3204184b967f7ad7db8a8b6bc4f
Rafael Nascimento <133170102+Rafaelndd@users.noreply.github.com>
2024-07-29 22:35:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ janela = Tk()
 janela.title("Calculadora")
 janela.geometry("243x350")
 janela.config(bg=cor1)
-janela.maxsize(243,350)
+janela.resizable(width=False, height=False)
 
 #   SEPARANDO TELA, CRIANDO DISPLAY #
 
-display_tela = Frame(janela, width=243, height=50, bg=cor4)
+display_tela = Frame(janela, width=243, height=50, bg=cor2)
 display_tela.grid(row=0, column=0)
 
 display_corpo = Frame(janela, width=243, height=300, bg=cor2)
@@ -66,7 +66,7 @@ def calcular():
 
 # CRIANDO LABEL #
 
-label_app = Label(display_tela, textvariable=valor_texto, width=23, height=3, padx=10, relief=FLAT, anchor="e", justify=RIGHT, font="Ivy 13 bold", bg=cor4, )
+label_app = Label(display_tela, textvariable=valor_texto, width=23, height=3, padx=10, relief=FLAT, anchor="e", justify=RIGHT, font="Ivy 13 bold", bg=cor2, )
 label_app.place(x=0, y=0)
 
 
@@ -80,9 +80,9 @@ label_app.place(x=0, y=0)
 
 b_1 = Button(display_corpo, command=limpar_tela,text="C", width=10, height=2, bg=cor3, fg=cor2, font=("Ivy 14 bold"), relief=RAISED, overrelief=RIDGE)
 b_1.place(x=3, y=3)
-b_1 = Button(display_corpo, command=lambda: entrada_valores("%"), text="%", width=4, height=2, font="Ivy 14 bold", relief=RAISED, overrelief=RIDGE)
+b_1 = Button(display_corpo, command=lambda: entrada_valores("%"), text="%",bg=cor3, width=4, height=2, font="Ivy 14 bold", relief=RAISED, overrelief=RIDGE)
 b_1.place(x=123, y=3)
-b_1 = Button(display_corpo, command=lambda: entrada_valores("/"), text="/", width=4, height=2, font="Ivy 14 bold", relief=RAISED, overrelief=RIDGE)
+b_1 = Button(display_corpo, command=lambda: entrada_valores("/"), text="/",bg=cor3, width=4, height=2, font="Ivy 14 bold", relief=RAISED, overrelief=RIDGE)
 b_1.place(x=183, y=3)
 
 # SEGUNDA LINHA DE BOTÕES #
@@ -104,7 +104,7 @@ b_7 = Button(display_corpo, command=lambda: entrada_valores("5"), text="5", widt
 b_7.place(x=63, y=123)
 b_8 = Button(display_corpo, command=lambda: entrada_valores("6"), text="6", width=4, height=2, font="Ivy 14 bold", relief=RAISED, overrelief=RIDGE)
 b_8.place(x=123, y=123)
-b_9 = Button(display_corpo, command=lambda: entrada_valores("-"), text="-", width=4, height=2, font="Ivy 14 bold", relief=RAISED, overrelief=RIDGE)
+b_9 = Button(display_corpo, command=lambda: entrada_valores("-"), text="-",bg=cor3, width=4, height=2, font="Ivy 14 bold", relief=RAISED, overrelief=RIDGE)
 b_9.place(x=183, y=120)
 
 # QUARTA LINHA DE BOTÕES #
@@ -115,7 +115,7 @@ b_11 = Button(display_corpo, command=lambda: entrada_valores("2"), text="2", wid
 b_11.place(x=63, y=183)
 b_12 = Button(display_corpo, command=lambda: entrada_valores("3"), text="3", width=4, height=2, font="Ivy 14 bold", relief=RAISED, overrelief=RIDGE)
 b_12.place(x=123, y=183)
-b_13 = Button(display_corpo, command=lambda: entrada_valores("+"), text="+", width=4, height=2, font="Ivy 14 bold", relief=RAISED, overrelief=RIDGE)
+b_13 = Button(display_corpo, command=lambda: entrada_valores("+"), text="+",bg=cor3, width=4, height=2, font="Ivy 14 bold", relief=RAISED, overrelief=RIDGE)
 b_13.place(x=183, y=180)
 
 # QUINTA LINHA DE BOTÕES #
@@ -124,7 +124,7 @@ b_14 = Button(display_corpo, command=lambda: entrada_valores("0"), text="0", wid
 b_14.place(x=3, y=241)
 b_15 = Button(display_corpo, command=lambda: entrada_valores("."), text=".", width=4, height=2, font="Ivy 14 bold", relief=RAISED, overrelief=RIDGE)
 b_15.place(x=123, y=241)
-b_16 = Button(display_corpo, command=calcular, text="=", width=4, height=2, font="Ivy 14 bold", relief=RAISED, overrelief=RIDGE)
+b_16 = Button(display_corpo, command=calcular, text="=",bg=cor3, width=4, height=2, font="Ivy 14 bold", relief=RAISED, overrelief=RIDGE)
 b_16.place(x=183, y=241)
 
 
